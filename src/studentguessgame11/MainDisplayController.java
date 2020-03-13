ed6fdd39400f0bccbc87748e9b525ca1690a8921
RajaAliHassan1 <60552815+RajaAliHassan1@users.noreply.github.com>
2020-03-13 11:44:00 +0500

    Update MainDisplayController.java

--- src/studentguessgame11/MainDisplayController.java
+++ src/studentguessgame11/MainDisplayController.java
@@ -24,7 +24,7 @@ import javafx.stage.Stage;
 /**
  * FXML Controller class
  *
- * @author Dawar Mustaqeem
+ * @author Raja Ali Hassan
  */
 public class MainDisplayController implements Initializable {
     @FXML
